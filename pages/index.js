35be0442662d62e53eeec373f8a1d65a30bd2b36
JT Seigfreid <jtseig@gmail.com>
2022-11-27 20:48:17 -0600

    latest build

---
+++ pages/index.js
@@ -0,0 +1,94 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
+  
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Create Web3 twitter threads</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Describe the crypto topics and concepts you want to cover!</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+          className="prompt-box"
+          placeholder="start typing here"
+          value={userInput}
+          onChange={onUserChangedText}
+          />;
+        </div>
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={callGenerateEndpoint}>
+          <div className="generate">
+          {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+      </div>
+    </a>
+  </div>
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-writer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build with buildspace</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
